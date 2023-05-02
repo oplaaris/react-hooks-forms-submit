b0648cdff78ab6060a4ed760c4e1c93924f74c6f
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-05-02 16:01:28 +0300

    lab attempted

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,13 +11,34 @@ function Form(props) {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+  function handleSubmit(event) {
+  event.preventDefault();
+  const formData = {firstName: e.target[0].value,lastName: e.target[1].value,};
+  const dataArray =[...submittedData, formData];
+  setSubmittedData(dataArray);
+
+  props.sendFormDataSomewhere(formData);
+  setFirstName("");
+  setLastName("");
+}
+const listOfSubmissions = submittedData.map((data,index)=>{
+  return(
+    <div key={index}>{data.firstName}{data.lastName}</div>
+  );
+});
 
   return (
-    <form>
+    <>
+    <form onSubmit = {handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    
+    {errors.length>0 ? errors.map((error,index )=>(<p key={index} style={{color:"red"}}>{error}</p>)):null}
+     <h3> Submissions</h3>
+    {listOfSubmissions}
+    </>
   );
 }
 
